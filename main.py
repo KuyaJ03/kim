50812fb8bd831a794b98697c3ac08f240fbb6faa
Yatss123 <olayvarricharddhale@gmail.com>
2025-03-14 22:55:38 +0800

    render

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Render!"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
